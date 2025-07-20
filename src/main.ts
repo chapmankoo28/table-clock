029e3c35d1676f04d17336f88ce192f5bd49ee34
chapmankoo28 <chap6758@gmail.com>
2025-07-20 16:55:11 +0800

    add Picocss from npm, config path aliases, switch to bun.lock

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,25 @@
 import { createApp } from "vue";
-import App from "./App.vue";
+import App from "@/App.vue";
 
-import router from './router';
-import { createPinia } from 'pinia'
-import i18n from './i18n'
-import './assets/pico.css'
-import './assets/main.css';
+import router from "@/router";
+import { createPinia } from "pinia";
+import i18n from "@/i18n";
 
-const pinia = createPinia()
+import '@picocss/pico/css/pico.min.css'
+import "@/assets/pico-custom.css";
+import "@/assets/main.css";
 
-const app = createApp(App)
-app.use(router)
-app.use(pinia)
-app.use(i18n)
+const pinia = createPinia();
+
+const app = createApp(App);
+app.use(router);
+app.use(pinia);
+app.use(i18n);
 app.mount("#app");
 
 // disables the right-click menu when the it is not in development mode.
 if (!import.meta.env.DEV) {
-    document.oncontextmenu = (event) => {
-        event.preventDefault()
-    }
+  document.oncontextmenu = (event) => {
+    event.preventDefault();
+  };
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
   plugins: [vue()],
-
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent vite from obscuring rust errors
@@ -18,25 +23,22 @@ export default defineConfig(() => ({
     host: host || false,
     hmr: host
       ? {
-        protocol: "ws",
-        host,
-        port: 1421,
-      }
+          protocol: "ws",
+          host,
+          port: 1421,
+        }
       : undefined,
     watch: {
       // 3. tell vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
     },
   },
-  envPrefix: ['VITE_', 'TAURI_ENV_*'],
+  envPrefix: ["VITE_", "TAURI_ENV_*"],
   build: {
     // Tauri uses Chromium on Windows and WebKit on macOS and Linux
-    target:
-      process.env.TAURI_ENV_PLATFORM == 'windows'
-        ? 'chrome105'
-        : 'safari13',
+    target: process.env.TAURI_ENV_PLATFORM == "windows" ? "chrome105" : "safari13",
     // don't minify for debug builds
-    minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' as const : false,
+    minify: !process.env.TAURI_ENV_DEBUG ? ("esbuild" as const) : false,
     // produce sourcemaps for debug builds
     sourcemap: !!process.env.TAURI_ENV_DEBUG,
   },
