4cdf8d1c4a739806c04d6f2ba498a29376341d18
Chapman Koo <51713693+chapmankoo28@users.noreply.github.com>
2026-08-01 18:43:33 +0800

    feat: move assets to styles and update Pico overrides

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,26 @@
-import { createApp } from "vue";
-import App from "@/App.vue";
+import App from '@/App.vue'
+import { createApp } from 'vue'
 
-import router from "@/router";
-import { createPinia } from "pinia";
-import i18n from "@/i18n";
+import i18n from '@/i18n'
+import router from '@/router'
+import { createPinia } from 'pinia'
 
 import '@picocss/pico/css/pico.min.css'
-import "@/assets/pico-custom.css";
-import "@/assets/main.css";
 
-const pinia = createPinia();
+import '@/styles/main.css'
+import '@/styles/pico-custom.css'
 
-const app = createApp(App);
-app.use(router);
-app.use(pinia);
-app.use(i18n);
-app.mount("#app");
+const pinia = createPinia()
+
+const app = createApp(App)
+app.use(router)
+app.use(pinia)
+app.use(i18n)
+app.mount('#app')
 
 // disables the right-click menu when the it is not in development mode.
 if (!import.meta.env.DEV) {
   document.oncontextmenu = (event) => {
-    event.preventDefault();
-  };
+    event.preventDefault()
+  }
 }
